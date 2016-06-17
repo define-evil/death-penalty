111160df13241e938fe8b7df299d2da8b9c9f043
randombyte-developer <apps.randombyte@gmail.com>
2016-06-17 23:04:08 +0200

    Added useful comments to the config

--- src/main/kotlin/de/randombyte/deathpenalty/Config.kt
+++ src/main/kotlin/de/randombyte/deathpenalty/Config.kt
@@ -6,8 +6,8 @@ import java.util.*
 
 @ConfigSerializable
 data class Config(
-        @Setting val xpReduction: String = "50%",
-        @Setting val moneyReduction: String = "0%",
+        @Setting(comment = "Can be relative or fixed => with or without percent sign") val xpReduction: String = "50%",
+        @Setting(comment = "Can be relative or fixed => with or without percent sign") val moneyReduction: String = "0%",
         @Setting(comment = "Time in seconds") val timeWithBlindness: Int = 180,
-        @Setting val recentlyDiedPlayers: List<UUID> = emptyList()
+        @Setting(comment = "Don't modify this value!") val recentlyDiedPlayers: List<UUID> = emptyList()
 )
